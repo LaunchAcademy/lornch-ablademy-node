d7da386a6986b1f1a918315f700b38a9b53defc9
Nick Alberts <nwalberts5@gmail.com>
2023-01-26 14:17:07 -0500

    done many to many

--- server/src/routes/api/v1/clinicQuestionsRouter.js
+++ server/src/routes/api/v1/clinicQuestionsRouter.js
@@ -2,8 +2,10 @@ import express from "express"
 import objection from "objection"
 const { ValidationError } = objection
 
-import { Question } from "../../../models/index.js"
+import { Clinic, Question } from "../../../models/index.js"
 import cleanUserInput from "../../../services/cleanUserInput.js"
+
+import QuestionSerializer from "../../../serializers/QuestionSerializer.js"
 
 const clinicQuestionsRouter = new express.Router({ mergeParams: true })
 
@@ -11,6 +13,14 @@ clinicQuestionsRouter.post("/", async (req, res) => {
   const { body } = req
   const formInput = cleanUserInput(body)
 
+  // formInput.clinicId = req.params.clinicId
+  const clinic = await Clinic.query().findById(req.params.clinicId)
+
+  const question = await clinic.$relatedQuery("questions").insertAndFetch(formInput)
+
+  const serializedQuestion = QuestionSerializer.getDetails(question)
+
+  return res.status(201).json({ question: serializedQuestion })
   // your code here
 })
 
